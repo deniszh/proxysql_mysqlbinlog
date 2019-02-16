6de4616d318c47c9b00206070e793d2f2485447f
deniszh <denis.zhdanov@gmail.com>
2019-02-16 22:36:59 +0100

    Trying to fix ssl mode

--- libslave/nanomysql.h
+++ libslave/nanomysql.h
@@ -95,7 +95,7 @@ public:
         const unsigned int connect_timeout = opts.mysql_connect_timeout;
         const unsigned int read_timeout = opts.mysql_read_timeout;
         const unsigned int write_timeout = opts.mysql_write_timeout;
-        const unsigned int arg_off = SSL_MODE_DISABLED;
+        enum mysql_ssl_mode ssl_mode = SSL_MODE_DISABLED;
         if (connect_timeout > 0)
         {
             mysql_options(connection, MYSQL_OPT_CONNECT_TIMEOUT, &connect_timeout);
@@ -121,7 +121,7 @@ public:
                      , nullptr
                      );
 */
-    mysql_options(connection, MYSQL_OPT_SSL_MODE, &arg_off);
+    mysql_options(connection, MYSQL_OPT_SSL_MODE, &ssl_mode);
     }
     Connection(const mysql_conn_opts& opts)
     {
